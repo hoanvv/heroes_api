2a0098766b7b9bc00c13d151c5dd63abb9935ff1
Hoan (Harley) V. VO <harley@HarleyBC11-Macmini.enclave.com>
2018-03-12 09:52:02 +0700

    Create API for getting package type list

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponse;
 
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host="localhost:8000",
+ *     basePath="/api/v1",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="This is my website cool API",
+ *         description="Api description...",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="contact@mysite.com"
+ *         ),
+ *         @SWG\License(
+ *             name="Private License",
+ *             url="URL to the license"
+ *         )
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Find out more about my website",
+ *         url="http..."
+ *     )
+ * )
+ */
+
 class ApiController extends Controller
 {
     use ApiResponse;
