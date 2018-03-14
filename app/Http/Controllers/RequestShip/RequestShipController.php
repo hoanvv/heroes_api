34e8a28511d1c1dc62d40cb5c7a0bda7b88c146b
Hoan (Harley) V. VO <harley@HarleyBC11-Macmini.enclave.com>
2018-03-14 09:17:31 +0700

    Update two field (pickup_location_address, destination_address) into request_ships table

--- app/Http/Controllers/RequestShip/RequestShipController.php
+++ app/Http/Controllers/RequestShip/RequestShipController.php
@@ -88,9 +88,11 @@ class RequestShipController extends ApiController
         $path = "package/available/{$requestShip->id}";
 
         $pickup_location = $requestShip->only('pickup_location');
-        $coordinate = json_decode($pickup_location['pickup_location'], true);
+        $data = json_decode($pickup_location['pickup_location'], true);
+        $extraData = $requestShip->only(['distance', 'destination_address', 'price']);
+        $data = array_merge($data, $extraData);
 
-        $this->saveDataWithoutAuthentication($path, $coordinate);
+        $this->saveDataWithoutAuthentication($path, $data);
 
         return $this->showOne($requestShip, 201);
     }
