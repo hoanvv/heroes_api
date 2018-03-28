f9d67ebbcb0d596cf52f517af5091ef807fc64dd
Hoan (Harley) V. VO <harley@HarleyBC11-Macmini.enclave.com>
2018-03-28 13:25:06 +0700

    Update sql query that gets a request ship

--- app/Entities/PackageOwner.php
+++ app/Entities/PackageOwner.php
@@ -44,19 +44,21 @@ class PackageOwner extends User
     {
         $sql = "";
         if ($isPicked) {
-            $sql = 'SELECT rs.*, u.first_name, u.last_name, u.phone, s.rating, s.avatar, rt1.status'
+            $sql = 'SELECT rs.*, u.first_name, u.last_name, u.phone, s.rating, s.avatar, rt1.status, pt.name as package_type'
                 . ' FROM users u'
                 . ' JOIN shippers s ON (u.id = s.user_id)'
                 . ' JOIN trips t ON (t.shipper_id = s.id)'
                 . ' JOIN request_ships rs ON (t.request_ship_id = rs.id)'
+                . ' JOIN package_types pt ON (rs.package_type_id = pt.id)'
                 . ' JOIN request_trackings rt1 ON (rs.id = rt1.request_ship_id)'
                 . ' LEFT OUTER JOIN request_trackings rt2'
                 . ' ON (rs.id = rt2.request_ship_id AND'
                 . ' (rt1.changed_at < rt2.changed_at OR rt1.changed_at = rt2.changed_at AND rt1.id < rt2.id))'
                 . ' WHERE rt2.id IS NULL AND rs.user_id = :user_id AND rs.id = :id';
         } else {
-            $sql = 'SELECT rs.*, rt1.status'
+            $sql = 'SELECT rs.*, rt1.status, pt.name as package_type'
                 . ' FROM request_ships rs'
+                . ' JOIN package_types pt ON (rs.package_type_id = pt.id)'
                 . ' JOIN request_trackings rt1 ON (rs.id = rt1.request_ship_id)'
                 . ' LEFT OUTER JOIN request_trackings rt2'
                 . ' ON (rs.id = rt2.request_ship_id AND'
