01a24d16113f20e99542837816fadb1e768000bc
Hoan (Harley) V. VO <harley@HarleyBC11-Macmini.enclave.com>
2018-04-02 09:45:27 +0700

    create api: update a shipper's current location

--- app/Http/Controllers/Shipper/ShipperController.php
+++ app/Http/Controllers/Shipper/ShipperController.php
@@ -44,7 +44,38 @@ class ShipperController extends ApiController
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = [
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric'
+        ];
+
+        $this->validate($request, $rules);
+        $data = $request->all();
+
+        $shipper = Auth::user()->shipper()->first();
+
+        if ($shipper->id != $id) {
+            $message = array(
+                'success' => false,
+                'message' => "You are cheated",
+                'code' => 401
+            );
+            $status = 401;
+        } else {
+            $shipper->latitude = $data['latitude'];
+            $shipper->longitude = $data['longitude'];
+
+            $shipper->save();
+
+            $message = array(
+                'success' => true,
+                'message' => "Updated the shipper's current location",
+                'code' => 200
+            );
+            $status = 200;
+        }
+
+        return response()->json($message, $status);
     }
 
 }
